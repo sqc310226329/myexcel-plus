19bb78fb1cd18d14977da371357b4837db764ad8
shiqiancheng <13285415306@163.com>
2019-12-17 11:17:52 +0800

    添加版本1

--- example/src/main/java/com/github/liaochong/example/controller/DefaultExcelBuilderExampleController.java
+++ example/src/main/java/com/github/liaochong/example/controller/DefaultExcelBuilderExampleController.java
@@ -29,6 +29,43 @@ public class DefaultExcelBuilderExampleController {
         AttachmentExportUtil.export(workbook, "艺术生信息", response);
     }
 
+    /**
+     * 根据传的列名进行导出
+     * @param response
+     * @throws Exception
+     */
+    @GetMapping("/customize/excel/example")
+    public void customizeBuild(HttpServletResponse response) throws Exception {
+        Field[] declaredFields = ArtCrowd.class.getDeclaredFields();
+        Arrays.stream(declaredFields).forEach(field->{
+            //获取val字段上的Foo注解实例
+            ExcelColumn foo = field.getAnnotation(ExcelColumn.class);
+            //获取 foo 这个代理实例所持有的 InvocationHandler
+            InvocationHandler h = Proxy.getInvocationHandler(foo);
+            // 获取 AnnotationInvocationHandler 的 memberValues 字段
+            Field hField = null;
+            try {
+                hField = h.getClass().getDeclaredField("memberValues");
+                // 因为这个字段事 private final 修饰，所以要打开权限
+                hField.setAccessible(true);
+                // 获取 memberValues
+                Map memberValues = (Map) hField.get(h);
+                //根据列名进行导出,相等导出
+                if (Objects.equals("需要导出的列名",field.getName())) {
+                    // 修改 value 属性值
+                    memberValues.put("isShow", true);//
+                }
+                // 获取 foo 的 value 属性值
+                String show = foo.isShow()+"";
+                System.out.println(show); // ddd
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        List<ArtCrowd> dataList = this.getDataList();
+        Workbook workbook = DefaultExcelBuilder.of(ArtCrowd.class).build(dataList);
+        AttachmentExportUtil.export(workbook, "艺术生信息", response);
+    }
     @GetMapping("/default/noStyle/example")
     public void defaultBuildWithNoStyle(HttpServletResponse response) throws Exception {
         List<ArtCrowd> dataList = this.getDataList();
